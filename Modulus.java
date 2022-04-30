bcab98653fb5de26e0f223a67625cf270247c7b4
Pavan696 <96572303+Pavan696@users.noreply.github.com>
2022-04-30 08:46:06 +0530

    import java.util.Scanner; class input { 	public static void main(String args[]) { 	int x,y,s; 	float av; 	Scanner sc=new Scanner(System.in); 	x=sc.nextInt(); 	y=sc.nextInt(); 	s=x%y;
          System.out.println(+s);
    	sc.close();
    	
    }}

---
+++ Modulus.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class input
+{
+	public static void main(String args[])
+{
+	int x,y,s;
+	float av;
+	Scanner sc=new Scanner(System.in);
+	x=sc.nextInt();
+	y=sc.nextInt();
+	s=x%y;
+	
+      System.out.println(+s);
+	sc.close();
+	
+}}
